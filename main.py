dd3bb2fef0c169f4f731c393d309d382bd23b020
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-20 23:03:23 -0800

    Adding tokens and creating the database

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey,DateTime
-from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-import datetime
+#import datetime
+from datetime import datetime, timezone
 
-USER = 'ROOT'
+USER = 'root'
 PASSWD = ''
 HOST = 'localhost'
 DATABASE = 'userDB'
 PORT = '3306'
 
-CONN = f"mysql+pymysql://{USER}:{PASSWD}@{HOST}:{PORT}/{DATABASE}"
+#CONN = f"sqlite+pymysql://{USER}:{PASSWD}@{HOST}:{PORT}/{DATABASE}"
+CONN = f"sqlite:///{DATABASE}.db"
+
 
 engine = create_engine(CONN, echo=True)
 Session = sessionmaker(bind=engine)
@@ -23,6 +27,15 @@ class Person(Base):
     user = Column(String(20))
     passwd = Column(String(10))
 
+class Tokens(Base):
+    __tablename__ = 'Tokens'
+    id = Column(Integer, primary_key = True)
+    id_person = Column(Integer, ForeignKey('Person.id'))
+    token = Column(String(100))
+    #date = Column(DateTime, default = datetime.datetime.utcnow())
+    date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+Base.metadata.create_all(engine)
 
 
 
