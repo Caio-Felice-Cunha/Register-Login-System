afcb98b9073a4d157868d5838cb7b8ba7e1f7396
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-20 23:31:09 -0800

    Starting the API. Defining connectDB and also registering in the database

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from fastapi import fastAPI
+from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import *
+from secrets import token_hex
+
+app = FastAPI()
+
+
+def connectDB():
+    engine = create_engine(CONN, echo = True)
+    Session = sessionmaker(bind = engine)
+    return Session()
+
+@app.post('/register')
+def register(name: str, user: str, passwd: str):
+    session = connectDB()
+    existing_user = session.query(Person).filter_by(user=user, passwd=passwd).all()
+    if len(existing_user) == 0:
+        x = Person(name=name, user=user, passwd=passwd)
+        session.add(x)
+        session.commit()
+        return {'status': 'success'}
+    elif len(existing_user) > 0:
+        return {'status': 'User already registered'}
+
+
