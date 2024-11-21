b8fe5723c1136a7095325fd2d99a7712beda7433
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-20 19:19:18 -0800

    Creating engine, session, base and Person class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
@@ -12,8 +12,16 @@ PORT = '3306'
 CONN = f"mysql+pymysql://{USER}:{PASSWD}@{HOST}:{PORT}/{DATABASE}"
 
 engine = create_engine(CONN, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 
-
+class Person(Base):
+    __tablename__ = 'Person'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(50))
+    user = Column(String(20))
+    passwd = Column(String(10))
 
 
 
