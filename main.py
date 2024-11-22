d2531fa2e3caaa38f21f66f679df5e095858fe9a
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-21 21:48:50 -0800

    Login and creating token

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tokenize import Token
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
@@ -24,5 +25,38 @@ def register(name: str, user: str, passwd: str):
         return {'status': 'success'}
     elif len(existing_user) > 0:
         return {'status': 'User already registered'}
+
+
+@app.post('/login')
+def login(user: str, passwd: str):
+    session = connectDB()
+    users = session.query(Person).filter_by(user = user, passwd = passwd).all()
+
+    if len(users) == 0:
+        return {'status': 'user not registered' } 
+
+    while True:
+        token = token_hex(50)
+        existingToken = session.query(Tokens).filter_by(token=token).all()
+
+        if len(existingToken) == 0:
+            existingPerson = session.query(Tokens).filter_by(id_person = users[0].id).all()
+            
+            if len(existingPerson) == 0:
+                newToken = Tokens(id_person = users[0].id, token=token)
+                session.add(newToken)
+
+            elif len(existingPerson) > 0:
+                existingPerson[0].token = token
+
+            session.commit()
+            break
+
+    return token
+
+            
+            
+
+
 
 
